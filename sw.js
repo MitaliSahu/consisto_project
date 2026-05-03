3fc3e22833a6393a5d35f4cfa627401102de1cbf
MitaliSahu <mitalisahu158@gmail.com>
2026-05-03 13:57:10 +0530

    Added smart install button for better experience

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
 const cacheName = 'consisto-v1';
-const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
